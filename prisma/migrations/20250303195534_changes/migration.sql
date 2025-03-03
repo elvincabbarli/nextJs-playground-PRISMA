ea9c5f63a0ff1b8fd9d425898f9ec16035065fe4
jabbarli <elvincabbarl@fmail.com>
2025-03-03 21:57:07 +0200

    Adapter problem fixed

---
+++ prisma/migrations/20250303195534_changes/migration.sql
@@ -0,0 +1,49 @@
+/*
+  Warnings:
+
+  - The primary key for the `user` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `createdAt` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `emailVerified` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `image` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `user` table. All the data in the column will be lost.
+  - You are about to alter the column `id` on the `user` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Int`.
+  - You are about to drop the `account` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `session` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `verificationrequest` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `account` DROP FOREIGN KEY `Account_userId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `session` DROP FOREIGN KEY `Session_userId_fkey`;
+
+-- AlterTable
+ALTER TABLE `user` DROP PRIMARY KEY,
+    DROP COLUMN `createdAt`,
+    DROP COLUMN `emailVerified`,
+    DROP COLUMN `image`,
+    DROP COLUMN `updatedAt`,
+    ADD COLUMN `followers` INTEGER NOT NULL DEFAULT 0,
+    ADD COLUMN `isActive` BOOLEAN NOT NULL DEFAULT true,
+    ADD COLUMN `registeredAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT,
+    ADD PRIMARY KEY (`id`);
+
+-- DropTable
+DROP TABLE `account`;
+
+-- DropTable
+DROP TABLE `session`;
+
+-- DropTable
+DROP TABLE `verificationrequest`;
+
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `price` DOUBLE NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
